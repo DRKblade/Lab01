00e85b1a847944a0097b51129962d068648b33cc
quandangv <quandangv@gmail.com>
2020-12-02 07:40:25 +0700

    finish project

--- 1712892_Lab01/ColorTransformer.h
+++ 1712892_Lab01/ColorTransformer.h
@@ -15,10 +15,10 @@ namespace ColorTransformer {
 	Hàm trả về:
 		trả ra 1 mảng chứa mảng các histogram
 	*/
-	vector<int[256]> Hist(const Mat& sourceImage) 
+	vector<array<int, 256>> Hist(const Mat& sourceImage) 
 	{
     //vector chưa từng màu của kênh màu
-		vector<int[256]> result; 
+		vector<array<int, 256>> result; 
     // ảnh gốc rỗng
 		if (sourceImage.empty())    
 			return move(result);
@@ -52,6 +52,30 @@ namespace ColorTransformer {
 		return move(result);
 	}
 
+	bool DrawHist(const Mat& src, Mat& dest) {
+		if (src.empty()) return false;// kiểm tra ảnh gốc
+		int n_channel = src.step[1];
+		dest = Mat(100*(n_channel + 1), 256, CV_8UC3);
+		auto hist = ColorTransformer::Hist(src);
+		if (dest.empty())return false;// kiểm tra ảnh kết quả
+		uchar* p_src = (uchar*)src.data;
+		uchar* p_dest = (uchar*)dest.data;
+
+		auto offset = 100*256*n_channel;
+		for (int i = 0; i < 100; i++) 
+		{
+			for (int j = 0; j < 256; j++)
+			{
+    		for (int c = 0; c < n_channel; c++) {
+    			auto norm = double(hist[c][j]) / double(src.cols * src.rows) * 20.0;
+  				*p_dest = p_dest[(c+1) * offset] = norm * 100 >= (100 - i) ? 255 : 0;
+  				p_dest++;
+    		}
+			}
+		}
+		return true;
+	}
+	
 	/*
 	Hàm so sánh hai ảnh
 	Tham so :
@@ -63,7 +87,7 @@ namespace ColorTransformer {
 	double CompareImage(const Mat& image1, const Mat& image2)
 	{
 		// Sử dụng độ đo Alternative Chi-Square
-		vector<int[256]> imageRGB_1, imageRGB_2;
+		vector<array<int, 256>> imageRGB_1, imageRGB_2;
 		int image1_chanel = image1.step[1];
 		int image2_chanel = image2.step[1];
 
@@ -83,6 +107,222 @@ namespace ColorTransformer {
 			}
 		}
 		return result;
+	}
+
+	/*
+	Hàm nhận vào một ảnh, thay đổi độ tương phản của ảnh này và lưu kết quả vào ảnh mới
+	Tham so :
+		sourceImage : ảnh ban đầu
+		destinationImage : ảnh kết quả
+		c	: giá trị số thực dùng để thay đổi độ tương phản của ảnh
+	Hàm trả về:
+		1: Nếu thành công thì trả về ảnh kết quả (ảnh gốc vẫn giữ nguyên giá trị)
+		0: Nếu không tạo được ảnh kết quả hoặc ảnh input không tồn tại
+	*/
+	int ChangeContrast(const Mat& sourceImage, Mat& destinationImage, float c)
+	{
+  	destinationImage = sourceImage.clone();
+ 
+		int height = sourceImage.rows;
+		int width = sourceImage.cols;
+		int srcChannels = sourceImage.channels();
+		int dstChannels = destinationImage.channels();
+		destinationImage = cv::Mat(height, width, CV_8UC3); // Tạo ảnh kết quả với kích thước bằng ảnh đầu vào
+
+		for (int x = 0; x < height; x++)
+		{
+			uchar* pSrcRow = (uchar*)(sourceImage.ptr<uchar>(x));
+			uchar* pDstRow = destinationImage.ptr<uchar>(x);
+			for (int y = 0; y < width; y++, pSrcRow += srcChannels, pDstRow += dstChannels)
+			{
+				// Nếu giá trị mức xám vượt quá 255 thì gán bằng 255
+				// pSrcRow[0] , pSrcRow[1] , pSrcRow[2] lần lượt là giá trị 3 kênh màu tương ứng B , G , R
+				if ((pSrcRow[0] * (uchar)(c)) > 255)
+				{
+					pDstRow[0] = 255;
+				}
+				else
+				{
+					pDstRow[0] = (uchar)((float)(pSrcRow[0]) * c);
+				}
+
+				if ((pSrcRow[1] * (uchar)(c)) > 255)
+				{
+					pDstRow[1] = 255;
+				}
+				else
+				{
+					pDstRow[1] = (uchar)((float)(pSrcRow[1]) * c);
+				}
+
+				if ((pSrcRow[2] * (uchar)(c)) > 255)
+				{
+					pDstRow[2] = 255;
+				}
+				else
+				{
+					pDstRow[2] = (uchar)((float)(pSrcRow[2]) * c);
+				}
+			}
+		}
+		return 0;
+	}
+
+	/*
+	Hàm cân bằng lược đồ màu tổng quát cho ảnh bất kỳ
+	Tham so :
+		image : ảnh dùng để tính histogram
+		histImage : ảnh histogram
+	Hàm trả về:
+		1: Nếu thành công vẽ được histogram
+		0: Nếu không vẽ được histogram
+	*/
+	int HistogramEqualization(const Mat& sourceImage, Mat& destinationImage)
+	{
+  	destinationImage = sourceImage.clone();
+
+		int height = sourceImage.rows;
+		int width = sourceImage.cols;
+		int srcChannels = sourceImage.channels();
+		int dstChannels = destinationImage.channels();
+		destinationImage = cv::Mat(height, width, CV_8UC3); // Tạo ma trận lưu ảnh kết quả với kích thước bằng kích thước ảnh đầu vào
+
+		// Tạo mảng H(2 chiều): bảng tần số phân bố xác suất các mức xám của ảnh đầu vào
+		// Với chiều rộng là 256 phần tử chứa các mức xám của ảnh từ 0-255
+		// Và chiều dài là 3 tương ứng với 3 kênh màu B , G , R tại mỗi điểm ảnh
+
+		int H[3][256];
+		// Tạo mảng T(2 chiều) : Bảng tần số phân bố xác suất các mức xám của ảnh sau khi cân bằng
+		// Với chiều rộng là 256 phần tử chứa các mức xám của ảnh từ 0-255
+		// Và chiều dài là 3 tương ứng với 3 kênh màu B , G , R tại mỗi điểm ảnh
+		int T[3][256];
+
+		// Tạo mảng Histogram(2 chiều) : Bảng giá trị các mức xám của ảnh sau khi cân bằng
+		// Với chiều rộng là 256 phần tử chứa các mức xám của ảnh từ 0-255
+		// Và chiều dài là 3 tương ứng với 3 kênh màu B , G , R tại mỗi điểm ảnh
+		int Histogram[3][256];
+
+		// Cho tất cả các giá trị của ma trận H,T,Histogram bằng 0
+		for (int c = 0; c < 3; c++)
+		{
+			for (int i = 0; i < 256; i++)
+			{
+				H[c][i] = 0;
+				T[c][i] = 0;
+				Histogram[c][i] = 0;
+			}
+		}
+
+		// Tính tần số phân  bố xác suất các mức xám của từng điểm ảnh trong ảnh đầu vào
+		for (int x = 0; x < height; x++)
+		{
+			uchar* pSrcRow = (uchar*)(sourceImage.ptr<uchar>(x));
+			for (int y = 0; y < width; y++, pSrcRow += srcChannels)
+			{
+				// f là giá trị mức xám từ 0-255
+				int f0 = (int)(pSrcRow[0]); // kênh màu B
+				int f1 = (int)(pSrcRow[1]); // kênh màu G
+				int f2 = (int)(pSrcRow[2]); // kênh màu R
+
+				// Khi giá trị pSrcRow bằng giá trị tương ứng của f thì giá trị tương ứng tại mảng H tăng lên 1
+				H[0][f0]++; 
+				H[1][f1]++;
+				H[2][f2]++;
+			}
+		}
+
+		//Gán giá trị đầu tiên của ma trận T = giá trị đầu tiên của ma trận H
+		T[0][0] = H[0][0];
+		T[1][0] = H[1][0];
+		T[2][0] = H[2][0];
+
+		// Tính tất cả các giá trị của ma trận T (Bảng phân bố tần số xác suất các mức xám của ảnh sau khi cân bằng)
+		for (int d = 0; d < 3; d++)
+		{
+			for (int p = 1; p < 256; p++)
+			{
+				T[d][p] = H[d][p] + T[d][(p - 1)];
+				Histogram[d][p] = ((255 * T[d][p]) / (height * width)); // Chuẩn hóa các giá trị điểm ảnh
+			}
+		}
+
+
+		// Ghép lại ảnh sau khi chuẩn hóa các giá trị mức xám theo bảng tần số phân bố xác suất
+
+		for (int x = 0; x < height; x++)
+		{
+			uchar* pSrcRow = (uchar*)(sourceImage.ptr<uchar>(x));
+			uchar* pDstRow = destinationImage.ptr<uchar>(x);
+			for (int y = 0; y < width; y++, pSrcRow += srcChannels, pDstRow += dstChannels)
+			{
+				pDstRow[0] = (uchar)(Histogram[0][pSrcRow[0]]);
+				pDstRow[1] = (uchar)(Histogram[1][pSrcRow[1]]);
+				pDstRow[2] = (uchar)(Histogram[2][pSrcRow[2]]);
+			}
+		}
+
+		return 1;
+	}
+
+	/*
+	Hàm nhận vào một ảnh, thay đổi độ sáng của ảnh này và lưu kết quả vào ảnh mới
+	Tham so:
+	sourceImage	: ảnh ban đầu
+	destinationImage: ảnh kết quả
+	b	: giá trị số nguyên dùng để thay đổi độ sáng của ảnh
+	Hàm trả về:
+	1: Nếu thành công thì trả về ảnh kết quả (ảnh gốc vẫn giữ nguyên giá trị)
+	0: Nếu không tạo được ảnh kết quả hoặc ảnh input không tồn tại
+	*/
+	int ChangeBrightness(const Mat& sourceImage, Mat& destinationImage, short b)
+	{
+		if (sourceImage.empty())
+			return 0;
+		destinationImage = sourceImage.clone();
+
+		int height = sourceImage.rows;
+		int width = sourceImage.cols;
+		int srcChannels = sourceImage.channels();
+		int dstChannels = destinationImage.channels();
+		destinationImage = cv::Mat(height, width, CV_8UC3); // Tạo ảnh kết quả với kích thước bằng ảnh đầu vào
+
+		for (int x = 0; x < height; x++)
+		{
+			uchar* pSrcRow = (uchar*)(sourceImage.ptr<uchar>(x));
+			uchar* pDstRow = destinationImage.ptr<uchar>(x);
+			for (int y = 0; y < width; y++, pSrcRow += srcChannels, pDstRow += dstChannels)
+			{
+				// Nếu giá trị mức xám vượt quá 255 thì gán bằng 255
+				// pSrcRow[0] , pSrcRow[1] , pSrcRow[2] lần lượt là giá trị 3 kênh màu tương ứng B , G , R
+				if ((pSrcRow[0] + (uchar)(b)) > 255)
+				{
+					pDstRow[0] = 255;
+				}
+				else
+				{
+					pDstRow[0] = (uchar)(pSrcRow[0] + b);
+				}
+
+				if ((pSrcRow[1] + (uchar)(b)) > 255)
+				{
+					pDstRow[1] = 255;
+				}
+				else
+				{
+					pDstRow[1] = (uchar)((float)(pSrcRow[1]) + b);
+				}
+
+				if ((pSrcRow[2] + (uchar)(b)) > 255)
+				{
+					pDstRow[2] = 255;
+				}
+				else
+				{
+					pDstRow[2] = (uchar)((float)(pSrcRow[2]) + b);
+				}
+			}
+		}
+		return 1;
 	}
 }
 
--- 1712892_Lab01/main.cpp
+++ 1712892_Lab01/main.cpp
@@ -3,7 +3,6 @@
 #include "opencv2/opencv.hpp"
 #include "opencv2/highgui/highgui.hpp"
 #include "ColorTransformer.h"
-#include "String"
 #include "Converter.h"
 
 using namespace cv;
@@ -13,36 +12,68 @@ void ShowImages(Mat src, Mat dest) {
 	namedWindow("Source Image");
 	imshow("Source Image", src);
 	namedWindow("Destination Image");
-	imshow("Destination Image", des);
-	waitKey(0);
+	imshow("Destination Image", dest);
+	while (waitKey(0) != 27);
 }
 
 int main(int argc, char** argv)
 {
-	Converter converter = Converter();
 	//<ProgramName.exe> <Command> <InputPath> <CommandArguments>
 	string command = string(argv[1]);
 	string path = string(argv[2]);
 	Mat source = imread(path, cv::IMREAD_COLOR);
-	Mat des;
+	Mat dest;
 	if (command.find("rgb2hsv") != -1)
 	{
-		if (Converter::RGB2HSV(source, des))
+		if (Converter::RGB2HSV(source, dest))
   		ShowImages(source, dest);
 		else cout << "Conversion failed";
-		return 0;
 	}
 	else if (command.find("hsv2rgb") != -1)
 	{
-		if (Converter::HSV2RGB(source, des))
+		if (Converter::HSV2RGB(source, dest))
   		ShowImages(source, dest);
 		else cout << "Conversion failed";
-		return 0;
+	}
+	else if (command.find("rgb2gray") != -1)
+	{
+		if (!Converter::RGB2GrayScale(source, dest))
+  		ShowImages(source, dest);
+		else cout << "Conversion failed";
+	}
+	else if (command.find("gray2rgb") != -1)
+	{
+		if (!Converter::GrayScale2RGB(source, dest))
+  		ShowImages(source, dest);
+		else cout << "Conversion failed";
 	}
 	else if (command.find("compare") != -1) 
 	{
 		string DesPath = string(argv[3]);
-		des = imread(DesPath, cv::IMREAD_COLOR);
-		cout << "Compare result: " << ColorTransformer::CompareImage(source, des);
+		dest = imread(DesPath, cv::IMREAD_COLOR);
+		cout << "Compare result: " << ColorTransformer::CompareImage(source, dest);
 	}
+	else if (command.find("drawhist") != -1)
+	{
+  	ColorTransformer::DrawHist(source, dest);
+  	ShowImages(source, dest);
+	}
+	else if (command.find("brightness") != -1)
+	{
+  	char** _;
+  	ColorTransformer::ChangeBrightness(source, dest, strtof(argv[3], _));
+  	ShowImages(source, dest);
+	}
+	else if (command.find("contrast") != -1)
+	{
+  	char** _;
+  	ColorTransformer::ChangeContrast(source, dest, strtof(argv[3], _));
+  	ShowImages(source, dest);
+	}
+	else if (command.find("equalhist") != -1)
+	{
+  	ColorTransformer::HistogramEqualization(source, dest);
+  	ShowImages(source, dest);
+	}
+	return 0;
 }
